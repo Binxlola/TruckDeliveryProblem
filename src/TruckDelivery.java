f9e4c55e312af942b10b74f3fab309d6e74699a5
Jason Smit <jason.michael.smit@gmail.com>
2020-09-20 03:23:49 +1200

    Committed

--- src/TruckDelivery.java
+++ src/TruckDelivery.java
@@ -5,8 +5,7 @@ import java.util.Random;
 public class TruckDelivery {
 
     public static Town[] route;
-    public static ArrayList<Object> data = new ArrayList<>();
-//    public static String[] actions = new String[5];
+    public static String[] actions;
 
 
     /**
@@ -29,19 +28,16 @@ public class TruckDelivery {
         Town currentTown = route[currentTownIdx];
         int dropOffProfit = currentTown.getDropOff() - currentLoadValue;
 
-        // Calculate the profits for the rest of the route trying each drop-off/pick-up option at the current town
-        // NOTE: can't drop off if there is no current load (no load value)
-        // NOTE: If these is no load the truck can pick up but there will be no profit gain
-        // NOTE: The Truck can always skip the current town, neither picking up nor dropping off. Profit will remain the same
-        int dropOff = currentLoadValue != 0 ? bruteForce(nextTown, 0, profit + dropOffProfit) : 0;
+        // Calculate the profits for the rest of the route trying each drop-off/pick-up option at the current townS
+        int dropOff = currentLoadValue != 0 ? bruteForce(nextTown, 0, profit + dropOffProfit) : profit;
         int pickUp = bruteForce(nextTown, currentTown.getPickup(), currentLoadValue != 0 ? profit + dropOffProfit: profit);
-        int skip = currentTownIdx != route.length ? bruteForce(nextTown, currentLoadValue, profit) : profit;
+        int skip = bruteForce(nextTown, currentLoadValue, profit);
 
-//        if(pickUp > dropOff && pickUp > skip) {
-//            actions[currentTownIdx] = "Picked up at town" + currentTownIdx;
-//        } else if (dropOff > pickUp && dropOff > skip) {
-//            actions[currentTownIdx] = "Dropped off at town" + currentTownIdx;
-//        }
+        if(pickUp > dropOff && pickUp > skip) {
+            actions[currentTownIdx] = "Picked up at town" + currentTownIdx;
+        } else if (dropOff > pickUp && dropOff > skip) {
+            actions[currentTownIdx] = "Dropped off at town" + currentTownIdx;
+        }
 
         return Math.max(skip, Math.max(pickUp,dropOff));
     }
@@ -50,7 +46,7 @@ public class TruckDelivery {
      * This is approximate approach to solve the problem of a truck that follows a linear route and either picks up or drops off
      * a load in order to receive the most profit from the route. This approach will look for local minimum for pickups and local
      * maximums for drops offs in order to determine the max profit possible for the route.
-     * @return
+     * @return The max profit or near max profit attainable on the current route
      */
     public static int approximateApproach() {
         int currentLoadValue = 0;
@@ -86,104 +82,141 @@ public class TruckDelivery {
     }
 
     public static int exactApproach() {
-        String[] actions = new String[route.length];
+        int[][] profitTable = new int[route.length - 1][route.length];
+        int currentMax = 0;
 
-        return findMax(0, route.length -1, 1);
+        for(int i = 0; i < profitTable.length; i++) { // Pickup town
+            int pickupValue = route[i].getPickup();
+            for(int j = i + 1; j < route.length; j++) { // Starting at first town where a drop is possible iterates all following drop points
+                int dropOffProfit = route[j].getDropOff() - pickupValue;
+                int previousProfit = i > 0 ? findPastMax(i,profitTable) : 0; // Highest profit obtained along route before current pickup town
+
+                if(i > 0) {
+                    profitTable[i][j] = dropOffProfit > 0 ? dropOffProfit + previousProfit : previousProfit;
+                } else {
+                    profitTable[i][j] = Math.max(dropOffProfit, 0);
+                }
+
+                int finalProfit = profitTable[i][j];
+                if(finalProfit > currentMax) { currentMax = finalProfit; }
+            }
+        }
+
+        return currentMax;
     }
 
-    public static int findMax(int start, int end, int gap) {
-        if(start == end) {return  0;}
-
+    private static int findPastMax(int pickupIdx, int[][] profitTable) {
         int currentMax = 0;
-        int mid = end;
 
-         for(int i = start; i < end; i++) {
-             int currentValue = route[i].getPickup();
-             int profit = i + gap <= end ? route[i + gap].getDropOff() - currentValue : 0;
-
-             if(profit > currentMax) {
-                currentMax = profit;
-                mid = i + gap;
-             }
-
-         }
-
-        return currentMax + findMax(start, mid - 1, gap + 1) + findMax(mid, end, gap);
+        for(int i = 0; i <= pickupIdx; i++) { // Start from first possible pickup town
+            for(int j = i + 1; j <= pickupIdx; j++) { // Start from first possible dropoff town
+                int profit = profitTable[i][j];
+                if(profit > currentMax) {currentMax = profit;}
+            }
+        }
+        return currentMax;
     }
 
     public static void createRoute(int nTowns) {
         route = new Town[nTowns];
         Random ran = new Random();
         for(int i = 0; i < nTowns; i++) {
-            route[i] = new Town(ran.nextInt(100), ran.nextInt(100));
+            int pickUp = ran.nextInt(100);
+            int dropOff;
+            do {
+                dropOff = ran.nextInt(100);
+            } while (dropOff > pickUp);
+            route[i] = new Town(pickUp, dropOff);
         }
     }
 
+    public static void run() {
+        long startTime;
+        long duration;
+        int profit;
+
+
+        System.out.println("=====Running Approximate Approach=====");
+        startTime = System.currentTimeMillis();
+        profit = approximateApproach();
+        duration = System.currentTimeMillis() - startTime;
+        System.out.println("Final Profit: " + profit);
+        System.out.println("completed in: " + (double)(duration / 1000) + " seconds\n");
+
+        System.out.println("=====Running Brute Force Approach=====");
+        startTime = System.currentTimeMillis();
+        profit = bruteForce(0,0,0);
+        duration = System.currentTimeMillis() - startTime;
+        System.out.println("Final Profit: " + profit);
+        System.out.println("completed in: " + (double)(duration / 1000) + " seconds\n");
+
+        System.out.println("=====Running Exact Approach=====");
+        startTime = System.currentTimeMillis();
+        profit = exactApproach();
+        duration = System.currentTimeMillis() - startTime;
+        System.out.println("Final Profit: " + profit);
+        System.out.println("completed in: " + (double)(duration / 1000) + " seconds");
+    }
+
+    public static void runTests() {
+
+        route = new Town[1];
+        actions = new String[1];
+        route[0] = new Town(100,80);
+        System.out.println("############## TEST 1 START ##############");
+        run();
+        System.out.println("############## TEST 1 END ##############\n");
+
+        route = new Town[2];
+        actions = new String[2];
+        route[0] = new Town(100, 80);
+        route[1] = new Town(120, 110);
+        System.out.println("############## TEST 2 START ##############");
+        run();
+        System.out.println("############## TEST 2 END ##############\n");
+
+        route = new Town[5];
+        actions = new String[5];
+        route[0] = new Town(100, 80);
+        route[1] = new Town(120, 110);
+        route[2] = new Town(140, 120);
+        route[3] = new Town(110, 90);
+        route[4] = new Town(180, 150);
+        System.out.println("############## TEST 3 START ##############");
+        run();
+        System.out.println("############## TEST 3 END ##############\n");
+
+        route = new Town[18];
+        actions = new String[18];
+        route[0] = new Town(140, 80);
+        route[1] = new Town(110, 100);
+        route[2] = new Town(120, 100);
+        route[3] = new Town(170, 120);
+        route[4] = new Town(110, 90);
+        route[5] = new Town(170, 80);
+        route[6] = new Town(140, 80);
+        route[7] = new Town(160, 110);
+        route[8] = new Town(150, 140);
+        route[9] = new Town(140, 140);
+        route[10] = new Town(160, 110);
+        route[11] = new Town(160, 110);
+        route[12] = new Town(110, 80);
+        route[13] = new Town(170, 130);
+        route[14] = new Town(110, 80);
+        route[15] = new Town(150, 130);
+        route[16] = new Town(120, 90);
+        route[17] = new Town(160, 80);
+        System.out.println("############## TEST 4 START ##############");
+        run();
+        System.out.println("############## TEST 4 END ##############\n");
+    }
+
     public static void main(String[] args) {
-        float startTime;
-        float endTime;
-//        data.add(0);
-//
-//        route = new Town[1];
-//        route[0] = new Town(100,80);
-//
-//        route = new Town[2];
-//        route[0] = new Town(100, 80);
-//        route[1] = new Town(120, 110);
+        // Run pre build tests
+        runTests();
 
-
-        // The route example posted on the discussion forum
-//        route = new Town[5];
-//        route[0] = new Town(100, 80);
-//        route[1] = new Town(120, 110);
-//        route[2] = new Town(140, 120);
-//        route[3] = new Town(110, 90);
-//        route[4] = new Town(180, 150);
-
-//        startTime = System.currentTimeMillis();
-//        System.out.println(bruteForce(0,0, 0));
-//        endTime = System.currentTimeMillis();
-//        System.out.println("completed in: " + (endTime - startTime) + " seconds");
-//
-//        startTime = System.currentTimeMillis();
-//        System.out.println(exactApproach());
-//        endTime = System.currentTimeMillis();
-//        System.out.println("completed in: " + (endTime - startTime) + " seconds");
-
-
-
-//        route = new Town[18];
-//        route[0] = new Town(140, 80);
-//        route[1] = new Town(110, 100);
-//        route[2] = new Town(120, 100);
-//        route[3] = new Town(170, 120);
-//        route[4] = new Town(110, 90);
-//        route[5] = new Town(170, 80);
-//        route[6] = new Town(140, 80);
-//        route[7] = new Town(160, 110);
-//        route[8] = new Town(150, 140);
-//        route[9] = new Town(140, 140);
-//        route[10] = new Town(160, 110);
-//        route[11] = new Town(160, 110);
-//        route[12] = new Town(110, 80);
-//        route[13] = new Town(170, 130);
-//        route[14] = new Town(110, 80);
-//        route[15] = new Town(150, 130);
-//        route[16] = new Town(120, 90);
-//        route[17] = new Town(160, 80);
-
-        createRoute(6);
-
-        startTime = System.currentTimeMillis();
-        System.out.println(bruteForce(0,0, 0));
-        System.out.println("completed in: " + (System.currentTimeMillis() - startTime) + " seconds");
-
-        startTime = System.currentTimeMillis();
-        System.out.println(exactApproach());
-        System.out.println("completed in: " + (System.currentTimeMillis() - startTime) + " seconds");
-
-//        System.out.println(bruteForce(0,0, 0));
-//        System.out.println(approximateApproach());
-//        System.out.println(exactApproach());
+        // Create random route with n towns and run test
+//        createRoute(20);
+//        run();
     }
 }
